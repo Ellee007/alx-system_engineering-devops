2fdbc1d185bc4e5d9f2a10eb8415af04d6aa6902
Ellee007 <alhassanaliyu83@gmail.com>
2024-02-25 06:45:42 -0800

    file 1n2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# Install a package using Puppet
+# Install flask 2.1.0 from pip3
 
-exec { 'puppet-lint':
-  command => '/bin/gem install puppet-lint -v 2.5.0',
-}
+package {'flask':
+    ensure   => '2.1.0',
+    provider => 'pip3'}
